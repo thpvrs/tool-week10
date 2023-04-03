c5d60d63f8a65bd50bdbb0853de286dd20931a44
thpvrs <63070104@kmitl.ac.th>
2023-04-03 23:52:39 +0700

    commit

--- backend/main.py
+++ backend/main.py
@@ -1,15 +1,24 @@
-
+from typing import List
 import uvicorn
 from fastapi import FastAPI
 from pydantic import BaseModel
 import numpy as np
 import cv2
 import base64
-from typing import List
-
-
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+
+origins = [""]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=[""],
+    allow_headers=["*"]
+)
+
 
 class ImageRequest(BaseModel):
     image: str
@@ -19,15 +28,18 @@ class ImageRequest(BaseModel):
 
 
 # encode image as base64 string
-def encode_image(image):
-    _, encoded_image = cv2.imencode(".jpg", image)
+def encodeimage(image):
+    , encoded_image = cv2.imencode(".jpg", image)
     return "data:image/jpeg;base64," + base64.b64encode(encoded_image).decode()
 
 # decode base64 string to image
+
+
 def decode_image(image_string):
     encoded_data = image_string.split(',')[1]
     nparr = np.frombuffer(base64.b64decode(encoded_data), np.uint8)
     return cv2.imdecode(nparr, cv2.IMREAD_COLOR)
+
 
 def apply_canny(image):
     gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
@@ -45,10 +57,3 @@ async def process_image(image_request: ImageRequest):
             "surname": image_request.surname,
             "numbers": image_request.numbers,
             "processed_image": processed_image}
-
-
-
-
-
-
-
