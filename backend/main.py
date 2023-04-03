fd2f98c71cf3c7e152bbd8bf5885bd0467ccdabc
thpvrs <63070104@kmitl.ac.th>
2023-04-03 23:59:56 +0700

    commit

--- backend/main.py
+++ backend/main.py
@@ -9,13 +9,13 @@ from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
 
-origins = [""]
+origins = ["*"]
 
 app.add_middleware(
     CORSMiddleware,
     allow_origins=origins,
     allow_credentials=True,
-    allow_methods=[""],
+    allow_methods=["*"],
     allow_headers=["*"]
 )
 
